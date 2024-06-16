80c070f56569c8fc981eef0b27b598c713cd01a8
billyflin <Billymartinezc@gmail.com>
2024-06-16 12:37:41 -0400

    Second not funcional yet

--- app/src/main/java/com/example/ufrosustentableapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/ufrosustentableapp/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Green30 = Color(0xFFA0DAB8)
+val Green50 = Color(0xFF6FCF97)
+val Green70 = Color(0xFF4AA96C)
+val Green90 = Color(0xFF2E854B)
+val Green100 = Color(0xFF006B3E)
+
+val GreenGrey30 = Color(0xFFC6EED3)
